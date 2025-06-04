40d0560d65725964dfa588feef03a2d0542440d3
Peer <122899247+PeerZ0@users.noreply.github.com>
2025-06-04 11:11:57 +0200

    Update main.py

--- Distributor/app/main.py
+++ Distributor/app/main.py
@@ -80,7 +80,7 @@ async def scrape_endpoint(
             "stealth": request.stealth,
             "cache": request.cache,
             "parse": request.parse,
-            "proxy_used": f"{result.get("proxy_used")}",
+            "proxy_used": result.get("proxy_used"),
             "runner_used": result.get("runner_id", "unknown"),
             "content": result,
         }
